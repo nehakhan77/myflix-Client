5a3fad00cc85ed0a4bd98a2ff2fac2aa63efd2e4
nehakhan77 <nehakhann71@gmail.com>
2024-01-09 13:20:19 -0500

    Fix bug

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,19 +3,21 @@ export const MovieView = ({ movie, onBackClick }) => {
     <div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>{" "}
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre.Name}</span>{" "}
+        {/* Accessing the Name property of the Genre object */}
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director.Name}</span>{" "}
+        {/* Accessing the Name property of the Director object */}
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
