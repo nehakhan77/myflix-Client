fcd4bd3eaf2374ac55927cc5ae096d9178c5455e
nehakhan77 <nehakhann71@gmail.com>
2024-01-10 12:59:08 -0500

    Updated MovieView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
     fetch("https://careerfoundry-movieflix-59ee318aca62.herokuapp.com/movies")
       .then((response) => response.json())
@@ -28,6 +30,10 @@ export const MainView = () => {
         setMovies(moviesFromApi);
       });
   }, []);
+
+   if (!user) {
+     return <LoginView />;
+   }
 
   if (selectedMovie) {
     let similarMovies = movies.filter((movie) => {
