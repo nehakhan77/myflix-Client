a2174de9d93847f00cbc2488cd4270b36948910c
nehakhan77 <nehakhann71@gmail.com>
2024-01-10 13:10:04 -0500

    created LoginView component

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
@@ -28,6 +30,11 @@ export const MainView = () => {
         setMovies(moviesFromApi);
       });
   }, []);
+
+   if (!user) {
+     return <LoginView />;
+   }
+
 
   if (selectedMovie) {
     return (
